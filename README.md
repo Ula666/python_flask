181961063fb87b3e73835815901eddd90f2f9b96
rrose666 <uziolko@hotmail.com>
2021-04-07 10:25:33 +0100

    added html files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-### Python Flask
-###MVC
+### MVC with Python Flask
+### MVC - Model , View, Controller
+
+
+
+- install flask
+`pip install flask`
+  
+- create a file called app.py
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# creating an app instance
+
+
+# use a decorator @ to define the route for our web page
+@app.route("/")
+# setting up a default page
+def index():
+    return " Welcome DevOps team"
+
+@app.route("/welcome/")
+def welcome():
+    return "Welcome on board"
+
+@app.route("/home/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# create two more pages/ routes and add the functionality for email and password
+# implement oop inheritance
+@app.route("/password/")
+def home():
+    return render_template("index.html")
+
